f76290fb40314a11d0b4b3891ee64506b1bc350f
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-03-28 04:19:33 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
