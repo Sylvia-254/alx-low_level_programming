284501fdfb056ace074512cf0ca8fcb0d5842838
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-03-30 02:09:22 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,26 +5,39 @@
  */
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int num1, num2, num3;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
-	for (tens = '0'; tens <= '9'; tens++)
-	for (ones = '0'; ones <= '9'; ones++)
+	num1 = '0';
+	num2 = '0';
+	num3 = '0';
+
+	while (num1 <= '9')
 	{
-		if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
-	}
-	{
-		putchar(hundreds);
-		putchar(tens);
-		putchar(ones);
-		if (!(ones == '9' && hundreds == '7' && tens == '8'))
-	}
-	{
-		putchar(',');
-		putchar(' ');
+		while (num2 <= '9')
+		{
+			num3 = '0';
+			while (num3 <= '9')
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+
+					if (num1 != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+		num2 = '0';
 	}
 	putchar('\n');
+
 	return (0);
 }
