c741f348506e9b34314e409b785c61388f9da2f8
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-02 06:25:46 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sum up two intergers
+ * Description: to add two intergers
+ * @a: the first interger
+ * @b: the second interger
+ * Return: (0)
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
