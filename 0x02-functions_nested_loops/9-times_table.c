9f71fbbe8387cd1ddd0de9743172d6a05ecff547
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-02 03:38:02 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - a function that prints 9
+ * times table
+ * Description: it prints followed by comma and 2 spaces
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 0; j <= 9; j++)
+		{
+			k = (i * j);
+
+			if ((k / 10) > 0)
+			{
+				_putchar((k / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+				_putchar((k % 10) + '0');
+			if (j > 9)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
