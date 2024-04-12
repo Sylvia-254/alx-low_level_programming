6e253f37b98f2c494138a96003014a72d061978e
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-12 14:05:12 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,13 @@ int t = 0;
 char *y = str;
 int o;
 
-while (y != '\0')
+while (*y != '\0')
 {
 y++;
 longi++;
 }
 t = longi - 1;
-for (o = 0; o <= t; o++)
+for (o = 0 ; o <= t ; o++)
 {
 if (o % 2 == 0)
 {
