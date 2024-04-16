f0046f3b158b033ad7d1774eeed0e09e693db381
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-16 09:54:34 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,17 +16,14 @@ unsigned int i;
 char *p;
 int cents[] = {25, 10, 5, 2};
 
-
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
 
-
 total = strtol(argv[1], &p, 10);
 count = 0;
-
 
 if (!*p)
 {
@@ -34,11 +31,11 @@ while (total > 1)
 {
 for (i = 0; i < sizeof(cents[i]); i++)
 {
-   if (total >= cents[i])
-   {
-       count += total / cents[i];
-       total = total % cents[i];
-   }
+if (total >= cents[i])
+{
+count += total / cents[i];
+total = total % cents[i];
+}
 }
 }
 if (total == 1)
@@ -49,7 +46,6 @@ else
 printf("Error\n");
 return (1);
 }
-
 
 printf("%d\n", count);
 return (0);
